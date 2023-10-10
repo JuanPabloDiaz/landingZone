6818f8fdaab05b5439a312a6c7a0a523611f096a
Juan Diaz <juanchodis@hotmail.com>
2023-10-09 22:34:46 -0400

    [ADD] Team Section - First commit from section

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -4,6 +4,7 @@ import Services from "./components/Services";
 import ServicesInfo from "./components/Services-info";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Team from "./components/Team";
 import Footer from "./components/Footer";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <ServicesInfo />
       <About />
       <Projects />
+      <Team />
       <Footer />
     </div>
   );
