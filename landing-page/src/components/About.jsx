3d3762908e09760631a6f6ce40f01ccff183beeb
Juan Diaz <juanchodis@hotmail.com>
2023-10-09 21:21:45 -0400

    [DONE] About Section

--- landing-page/src/components/About.jsx
+++ landing-page/src/components/About.jsx
@@ -4,10 +4,10 @@ export default () => {
       <div className="max-w-screen-xl mx-auto px-4 text-gray-600 gap-16 justify-between md:px-8 lg:flex">
         <div>
           <div className="max-w-xl space-y-3">
-            <h3 className="text-indigo-600 font-semibold">
+            <h3 className="text-[#132577] font-semibold text-xl">
               FEW WORDS ABOUT US
             </h3>
-            <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+            <p className="text-gray-800 text-3xl font-bold sm:text-2xl">
               We Are Leaders in It Solutions
             </p>
             <p>
@@ -18,19 +18,28 @@ export default () => {
               pretium, sit amet finibus ex.
             </p>
           </div>
-          <div className="mt-8 max-w-lg lg:max-w-none">
-            <ul className="space-y-8">
-              <li>Quisque metus felis in dictum</li>
-              <li>Mauris sollicitudin nunc quis at</li>
-              <li>Sed ac ligula nec felis modo</li>
-              <li>In massa erat, ac dictum alis</li>
+          <div className="mt-6 max-w-lg lg:max-w-none">
+            <ul className="space-y-4 flex flex-col md:flex-row md:flex-wrap md:space-x-3">
+              <li className="list-disc list-inside text-black font-semibold">
+                Quisque metus felis in dictum 1.
+              </li>
+              <li className="list-disc list-inside text-black font-semibold">
+                Mauris sollicitudin nunc quis at 2.
+              </li>
+              <li className="list-disc list-inside text-black font-semibold">
+                Sed ac ligula nec felis modo 3.
+              </li>
+              <li className="list-disc list-inside text-black font-semibold">
+                In massa erat, ac dictum alis 4.
+              </li>
             </ul>
           </div>
         </div>
         <div className="mt-12 lg:mt-0">
           <img
-            src="https://res.cloudinary.com/floatui/image/upload/v1670150563/desktop_dte2ar.png"
-            className="w-full shadow-lg rounded-lg border"
+            src={require("../assets/img/2-aboutUs582.png")}
+            alt="about us"
+            className="max-w-xl hidden md:flex "
           />
         </div>
       </div>
