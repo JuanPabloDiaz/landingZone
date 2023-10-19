44574d764cd5efa90efc13e530e4fd8e55c8205f
Juan Diaz <juanchodis@hotmail.com>
2023-10-19 14:46:00 -0400

    MVP2 >> COMPANY: add paragraph

--- landing-page/src/components/Company.jsx
+++ landing-page/src/components/Company.jsx
@@ -25,10 +25,10 @@ const OpenCards = () => {
     "Our Customer",
   ];
   const cardDescriptions = [
-    "this is a description, with more text than the other cards",
-    "this is a description, with more text than the other cards",
-    "this is a description, with more text than the other cards",
-    "this is a description, with more text than the other cards",
+    "To foster brand success by delivering innovative software solutions that help them thrive and distinguish themselves.",
+    "To empower every brand to define new standards of excellence and distinction, using our software solutions.",
+    "We're dedicated to fueling brand success with cutting-edge technology, making us the catalyst for your journey to excellence.",
+    "Our customers are ambitious businesses and entrepreneurs, and we're here to empower their success with innovative software solutions.",
   ];
   return (
     <section name="company" className="py-16 pb-16">
