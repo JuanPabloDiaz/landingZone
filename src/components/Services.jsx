967d7849c4f9bfd71a80f2eed9a7be6b848ac1d9
Juan Diaz <juanchodis@hotmail.com>
2023-10-22 12:44:56 -0400

    testing services section on mobile

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -42,20 +42,20 @@ const posts = [
 
 export default function Example() {
   return (
-    <div name="services" className="bg-white pt-24 pb-6 sm:pt-32 sm:pb-6">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto lg:mx-0 text-center">
-          <h2 className="text-4xl tracking-tight text-[#132577] sm:text-2xl">
+    <div name="services" className="pt-24 pb-6 sm:pt-32 sm:pb-6">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 bg-slate-500">
+        <div className="mx-auto lg:mx-0 text-center bg-slate-900">
+          <h2 className="text-4xl tracking-tight text-[#132577] sm:text-2xl bg-amber-800">
             SERVICES
           </h2>
           <p className="mt-2 text-3xl font-bold leading-8 text-black-600">
             We Provide All-In-One Solution
           </p>
-          <p className="mt-2 text-3xl font-bold leading-8 text-black-600">
+          <p className="text-3xl font-bold leading-8 text-black-600">
             For Every IT Job
           </p>
         </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-4">
           {posts.map((post) => (
             <article
               key={post.id}
