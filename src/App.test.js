d64fe30e73cb65d202039f5f5d87265d0a6b0254
JuanPabloDiaz <juanchodis@hotmail.com>
2024-07-17 08:25:06 -0400

    passed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders button component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/explore more/i);
   expect(linkElement).toBeInTheDocument();
 });
